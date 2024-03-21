e3de4cdc653818d109e58ced8aae1d9d99e75b9b
htmtmx <cesar.pineda@daksha.mx>
2024-03-21 16:06:55 -0600

    ♻️ Refactor Header component to use Fragment

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,13 @@
-import React from "react";
+import { Fragment } from "react";
 
 export default function Header() {
 
   const username = 'htmtmx';
 
   return (
-    <React.Fragment>
+    <Fragment>
       <p>{username}</p>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam laborum adipisci suscipit inventore laboriosam cupiditate pariatur corporis. Saepe nisi ex facilis, esse id minima architecto eligendi. Corporis modi praesentium doloribus!</p>
-    </React.Fragment>
+    </Fragment>
   );
 }
