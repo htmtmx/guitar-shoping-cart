badbb8e01b3323e42a18c1c375f929c4bad44ea2
htmtmx <cesar.pineda@daksha.mx>
2024-03-21 16:06:21 -0600

    💄Add React import and change fragment syntax in Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,13 @@
+import React from "react";
+
 export default function Header() {
 
   const username = 'htmtmx';
 
   return (
-    <>
+    <React.Fragment>
       <p>{username}</p>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam laborum adipisci suscipit inventore laboriosam cupiditate pariatur corporis. Saepe nisi ex facilis, esse id minima architecto eligendi. Corporis modi praesentium doloribus!</p>
-    </>
+    </React.Fragment>
   );
 }
